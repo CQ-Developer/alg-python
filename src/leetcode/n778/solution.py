bd7b2947dddc46a24450e5c16f613cc8560281a6
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-05-19 01:26:07 +0000

    778 swim in rising water binary search + dfs

--- src/leetcode/n778/solution.py
+++ src/leetcode/n778/solution.py
@@ -1,8 +1,39 @@
 from abc import ABC, abstractmethod
+from typing import override
 
 
 class Solution(ABC):
 
     @abstractmethod
-    def swinInWater(self, grid: list[list[int]]) -> int:
+    def swimInWater(self, grid: list[list[int]]) -> int:
         pass
+
+
+class SolutionA(Solution):
+
+    @override
+    def swimInWater(self, grid: list[list[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+
+        def check(vis: list[list[bool]], i: int, j: int, mx: int) -> bool:
+            if i == m - 1 and j == n - 1:
+                return True
+            vis[i][j] = True
+            if i > 0 and not vis[i - 1][j] and grid[i - 1][j] <= mx and check(vis, i - 1, j, mx):
+                return True
+            if i + 1 < m and not vis[i + 1][j] and grid[i + 1][j] <= mx and check(vis, i + 1, j, mx):
+                return True
+            if j > 0 and not vis[i][j - 1] and grid[i][j - 1] <= mx and check(vis, i, j - 1, mx):
+                return True
+            if j + 1 < n and not vis[i][j + 1] and grid[i][j + 1] <= mx and check(vis, i, j + 1, mx):
+                return True
+            return False
+
+        l, r = grid[0][0], max(max(g) for g in grid)
+        while l < r:
+            mid = (l + r) // 2
+            if check([[False for _ in range(n)] for _ in range(m)], 0, 0, mid):
+                r = mid
+            else:
+                l = mid + 1
+        return r
--- test/leetcode/n778/test_solution.py
+++ test/leetcode/n778/test_solution.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from src.leetcode.n778.solution import Solution
+from src.leetcode.n778.solution import Solution, SolutionA
 
 
 class SolutionTest(TestCase):
@@ -9,10 +9,16 @@ class SolutionTest(TestCase):
         self.skipTest('skip')
 
     def test_1(self):
-        self.assertEqual(3, self.solution.swinInWater([[0, 2], [1, 3]]))
+        self.assertEqual(3, self.solution.swimInWater([[0, 2], [1, 3]]))
 
     def test_2(self):
-        self.assertEqual(16, self.solution.swinInWater([[0, 1, 2, 3, 4], [24, 23, 22, 21, 5], [12, 13, 14, 15, 16], [11, 17, 18, 19, 20], [10, 9, 8, 7, 6]]))
+        self.assertEqual(16, self.solution.swimInWater([[0, 1, 2, 3, 4], [24, 23, 22, 21, 5], [12, 13, 14, 15, 16], [11, 17, 18, 19, 20], [10, 9, 8, 7, 6]]))
 
     def test_3(self):
-        self.assertEqual(11, self.solution.swinInWater([[11, 15, 3, 2], [6, 4, 0, 13], [5, 8, 9, 10], [1, 14, 12, 7]]))
+        self.assertEqual(11, self.solution.swimInWater([[11, 15, 3, 2], [6, 4, 0, 13], [5, 8, 9, 10], [1, 14, 12, 7]]))
+
+
+class SolutionATest(SolutionTest):
+
+    def setUp(self):
+        self.solution = SolutionA()
