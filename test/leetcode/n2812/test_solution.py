0cab3eb15ef51f2f06d653d65238da130649054e
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-05-26 03:12:13 +0000

    2812 find the safest path in a grid

---
+++ test/leetcode/n2812/test_solution.py
@@ -0,0 +1,19 @@
+from src.leetcode.n2812.solution import Solution
+from unittest import TestCase
+
+
+class SolutionTest(TestCase):
+
+    solution: Solution
+
+    def setUp(self):
+        self.skipTest('skip')
+
+    def test_1(self):
+        self.assertEqual(0, self.solution.maximumSafenessFactor([[1, 0, 0], [0, 0, 0], [0, 0, 1]]))
+
+    def test_2(self):
+        self.assertEqual(2, self.solution.maximumSafenessFactor([[0, 0, 1], [0, 0, 0], [0, 0, 0]]))
+
+    def test_3(self):
+        self.assertEqual(2, self.solution.maximumSafenessFactor([[0, 0, 0, 1], [0, 0, 0, 0], [0, 0, 0, 0], [1, 0, 0, 0]]))
