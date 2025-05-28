5eee1c2572514690190a2d37e1f7c9feda2cb33f
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-05-28 06:04:36 +0000

    2812 find the safest path in a grid bfs + union search

--- src/leetcode/n2812/solution.py
+++ src/leetcode/n2812/solution.py
@@ -48,3 +48,45 @@ class SolutionA(Solution):
             else:
                 r = m - 1
         return r
+
+
+class SolutionB(Solution):
+
+    @override
+    def maximumSafenessFactor(self, grid: list[list[int]]) -> int:
+        n = len(grid)
+
+        # 并查集
+        f = list(range(n * n))
+
+        def find(x: int) -> int:
+            if f[x] != x:
+                f[x] = find(f[x])
+            return f[x]
+
+        # 多源BFS
+        q: list[tuple[int, int]] = []
+        dis = [[-1] * n for _ in range(n)]
+        for i, row in enumerate(grid):
+            for j, x in enumerate(row):
+                if x:
+                    q.append((i, j))
+                    dis[i][j] = 0
+        groups = [q]
+        while q:
+            tmp = q
+            q = []
+            for i, j in tmp:
+                for x, y in (i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1):
+                    if 0 <= x < n and 0 <= y < n and dis[x][y] == -1:
+                        q.append((x, y))
+                        dis[x][y] = len(groups)
+            groups.append(q)
+        for g in range(len(groups) - 2, 0, -1):
+            for i, j in groups[g]:
+                for x, y in (i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1):
+                    if 0 <= x < n and 0 <= y < n and dis[x][y] >= dis[i][j]:
+                        f[find(x * n + y)] = find(i * n + j)
+            if find(0) == find(n * n - 1):
+                return g
+        return 0
