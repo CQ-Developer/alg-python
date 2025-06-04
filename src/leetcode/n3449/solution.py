fadf60aa238ac075c9e1ce1f1cc0c2fcc4bd7cb1
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-06-04 04:00:00 +0000

    3449 maximize the minimum game score binary search + greedy

--- src/leetcode/n3449/solution.py
+++ src/leetcode/n3449/solution.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from typing import override
 
 
 class Solution(ABC):
@@ -6,3 +7,33 @@ class Solution(ABC):
     @abstractmethod
     def maxScore(self, points: list[int], m: int) -> int:
         pass
+
+
+class SolutionA(Solution):
+
+    @override
+    def maxScore(self, points: list[int], m: int) -> int:
+        n = len(points)
+
+        def check(low: int) -> bool:
+            p, r = 0, m
+            for i, x in enumerate(points):
+                k = (low - 1) // x + 1 - p
+                if i == n - 1 and k <= 0:
+                    break
+                if k < 1:
+                    k = 1
+                r -= 2 * k - 1
+                if r < 0:
+                    return False
+                p = k - 1
+            return True
+
+        l, r = 0, (m + 1) // 2 * min(points) + 1
+        while l + 1 < r:
+            mid = (l + r) // 2
+            if check(mid):
+                l = mid
+            else:
+                r = mid
+        return l
--- test/leetcode/n3449/test_solution.py
+++ test/leetcode/n3449/test_solution.py
@@ -1,4 +1,4 @@
-from src.leetcode.n3449.solution import Solution
+from src.leetcode.n3449.solution import Solution, SolutionA
 from unittest import TestCase
 
 
@@ -14,3 +14,9 @@ class SolutionTest(TestCase):
 
     def test_2(self):
         self.assertEqual(2, self.solution.maxScore([1, 2, 3], 5))
+
+
+class SolutionATest(SolutionTest):
+
+    def setUp(self):
+        self.solution = SolutionA()
