bb3b0e50768a8e3c54894f606514d8f8363bd96e
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-06-06 10:00:05 +0000

    3464 maximize the distance between points on a square binary search

--- src/leetcode/n3464/solution.py
+++ src/leetcode/n3464/solution.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from typing import override
 from itertools import pairwise
+from bisect import bisect_left
 
 
 class Solution(ABC):
@@ -41,6 +42,54 @@ class SolutionA(Solution):
             idx = [0] * k
             for i, j in pairwise(range(k)):
                 f = find(arr[idx[i]] + low)
+                if f == n:
+                    return False
+                idx[j] = f
+            if arr[idx[-1]] - arr[0] <= 4 * side - low:
+                return True
+            for i in range(1, idx[1]):
+                idx[0] = i
+                for p, j in pairwise(range(k)):
+                    while arr[idx[j]] - arr[idx[p]] < low:
+                        idx[j] += 1
+                        if idx[j] == n:
+                            return False
+                if arr[idx[-1]] - arr[i] <= 4 * side - low:
+                    return True
+            return False
+
+        l, r = 1, (4 * side) // k + 1
+        while l + 1 < r:
+            mid = (l + r) // 2
+            if check(mid):
+                l = mid
+            else:
+                r = mid
+        return l
+
+
+class SolutionB(Solution):
+
+    @override
+    def maxDistance(self, side: int, points: list[list[int]], k: int) -> int:
+        n = len(points)
+        arr: list[int] = []
+        for x, y in points:
+            if x == 0:
+                arr.append(y)
+            elif y == side:
+                arr.append(side + x)
+            elif x == side:
+                arr.append(3 * side - y)
+            else:
+                arr.append(4 * side - x)
+        arr.sort()
+
+        def check(low: int) -> bool:
+            print(low)
+            idx = [0] * k
+            for i, j in pairwise(range(k)):
+                f = bisect_left(arr, arr[idx[i]] + low)
                 if f == n:
                     return False
                 idx[j] = f
