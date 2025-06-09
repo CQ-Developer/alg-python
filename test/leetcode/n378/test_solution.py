b8a5a280cf7b6a44083e4a1e3ccf54a7cf42eb4f
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-06-09 01:22:11 +0000

    378 kth smallest element in a sorted matrix

---
+++ test/leetcode/n378/test_solution.py
@@ -0,0 +1,16 @@
+from src.leetcode.n378.solution import Solution
+from unittest import TestCase
+
+
+class SoutionTest(TestCase):
+
+    solution: Solution
+
+    def setUp(self):
+        self.skipTest('skip')
+
+    def test_1(self):
+        self.assertEqual(13, self.solution.kthSmallest([[1, 5, 9], [10, 11, 13], [12, 13, 15]], 8))
+
+    def test_2(self):
+        self.assertEqual(-5, self.solution.kthSmallest([[-5]], 1))
