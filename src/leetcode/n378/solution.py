8c18b628cb512a2b063c6c8a0e870870742e3414
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-06-10 02:41:11 +0000

    378 kth smallest element in a sorted matrix merge sort

--- src/leetcode/n378/solution.py
+++ src/leetcode/n378/solution.py
@@ -1,3 +1,4 @@
+import heapq
 from abc import ABC, abstractmethod
 from typing import override
 from itertools import chain
@@ -17,3 +18,17 @@ class SolutionA(Solution):
     def kthSmallest(self, matrix: list[list[int]], k: int) -> int:
         arr = SortedList(chain.from_iterable(matrix))
         return arr[k - 1]
+
+
+class SolutionB(Solution):
+
+    @override
+    def kthSmallest(self, matrix: list[list[int]], k: int) -> int:
+        n = len(matrix)
+        q = [(matrix[i][0], i, 0) for i in range(n)]
+        heapq.heapify(q)
+        for _ in range(k - 1):
+            _, i, j = heapq.heappop(q)
+            if j + 1 < n:
+                heapq.heappush(q, (matrix[i][j + 1], i, j + 1))
+        return heapq.heappop(q)[0]
--- test/leetcode/n378/test_solution.py
+++ test/leetcode/n378/test_solution.py
@@ -1,4 +1,4 @@
-from src.leetcode.n378.solution import Solution, SolutionA
+from src.leetcode.n378.solution import Solution, SolutionA, SolutionB
 from unittest import TestCase
 
 
@@ -10,7 +10,8 @@ class SolutionTest(TestCase):
         self.skipTest('skip')
 
     def test_1(self):
-        self.assertEqual(13, self.solution.kthSmallest([[1, 5, 9], [10, 11, 13], [12, 13, 15]], 8))
+        self.assertEqual(13, self.solution.kthSmallest(
+            [[1, 5, 9], [10, 11, 13], [12, 13, 15]], 8))
 
     def test_2(self):
         self.assertEqual(-5, self.solution.kthSmallest([[-5]], 1))
@@ -20,3 +21,9 @@ class SolutionATest(SolutionTest):
 
     def setUp(self):
         self.solution = SolutionA()
+
+
+class SolutionBTest(SolutionTest):
+
+    def setUp(self):
+        self.solution = SolutionB()
