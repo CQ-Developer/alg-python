8560c4cc38515d49360c4407bada4af2c1edbbcf
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-06-16 06:18:13 +0000

    793. preimage size of factorial zeroes function binary search + math

--- src/leetcode/n793/solution.py
+++ src/leetcode/n793/solution.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from typing import override
 
 
 class Solution(ABC):
@@ -6,3 +7,23 @@ class Solution(ABC):
     @abstractmethod
     def preimageSizeFZF(self, k: int) -> int:
         pass
+
+
+class SolutionA(Solution):
+
+    @override
+    def preimageSizeFZF(self, k: int) -> int:
+        l, r = 0, 5 * k
+        while l <= r:
+            mid = (l + r) // 2
+            n, cnt = 5, 0
+            while n <= mid:
+                cnt += mid // n
+                n *= 5
+            if cnt == k:
+                return 5
+            if cnt < k:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return 0
--- test/leetcode/n793/test_solution.py
+++ test/leetcode/n793/test_solution.py
@@ -1,4 +1,4 @@
-from src.leetcode.n793.solution import Solution
+from src.leetcode.n793.solution import Solution, SolutionA
 from unittest import TestCase
 
 
@@ -17,3 +17,9 @@ class SolutionTest(TestCase):
 
     def test_3(self):
         self.assertEqual(5, self.solution.preimageSizeFZF(3))
+
+
+class SolutionATest(SolutionTest):
+
+    def setUp(self):
+        self.solution = SolutionA()
