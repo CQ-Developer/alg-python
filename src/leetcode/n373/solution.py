c33539b68c2e9e425ba6be02c1160a063a36dc62
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-06-18 06:43:34 +0000

    373. find k pairs with smallest sums binary search

--- src/leetcode/n373/solution.py
+++ src/leetcode/n373/solution.py
@@ -40,3 +40,48 @@ class SolutionB(Solution):
             if j + 1 < n:
                 heapq.heappush(hp, (nums1[i] + nums2[j + 1], i, j + 1))
         return res
+
+
+class SolutionC(Solution):
+
+    @override
+    def kSmallestPairs(self, nums1: list[int], nums2: list[int], k: int) -> list[list[int]]:
+        m, n = len(nums1), len(nums2)
+
+        def check(x: int) -> int:
+            cnt, l, r = 0, 0, n - 1
+            while l < m and r >= 0:
+                if nums1[l] + nums2[r] > x:
+                    r -= 1
+                else:
+                    l += 1
+                    cnt += r + 1
+            return cnt
+
+        left, right = nums1[0] + nums2[0], nums1[-1] + nums2[-1]
+        while left <= right:
+            mid = (left + right) // 2
+            if check(mid) >= k:
+                right = mid - 1
+            else:
+                left = mid + 1
+        res: list[list[int]] = []
+        r = n - 1
+        for x in nums1:
+            while r >= 0 and x + nums2[r] >= left:
+                r -= 1
+            for y in islice(nums2, r + 1):
+                res.append([x, y])
+                if len(res) == k:
+                    return res
+        r = n - 1
+        for x in nums1:
+            while r >= 0 and x + nums2[r] > left:
+                r -= 1
+            i = r
+            while i >= 0 and x + nums2[i] == left:
+                res.append([x, nums2[i]])
+                if len(res) == k:
+                    return res
+                i -= 1
+        return res
