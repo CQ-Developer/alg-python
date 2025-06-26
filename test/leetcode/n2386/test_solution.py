f652ca56c8244069bba62954539e1e591b0931a3
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-06-26 03:09:05 +0000

    2386. find the k-sum of an array

---
+++ test/leetcode/n2386/test_solution.py
@@ -0,0 +1,15 @@
+from src.leetcode.n2386.solution import Solution
+from unittest import TestCase
+
+
+class SolutionTest(TestCase):
+    solution: Solution
+
+    def setUp(self):
+        self.skipTest('skip')
+
+    def test_1(self):
+        self.assertEqual(2, self.solution.k_sum([2, 4, -2], 5))
+
+    def test_2(self):
+        self.assertEqual(10, self.solution.k_sum([1, -2, 3, 4, -10, 12], 16))
