83eda071d70f463ab2dc9c87c81b3928475bab46
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-06-26 07:16:53 +0000

    2386. find the k-sum of an array: binary search + dfs

--- src/leetcode/n2386/solution.py
+++ src/leetcode/n2386/solution.py
@@ -1,6 +1,7 @@
 import heapq
 from abc import ABC, abstractmethod
 from typing import override
+from bisect import bisect_left
 
 
 class Solution(ABC):
@@ -29,3 +30,33 @@ class SolutionA(Solution):
                 heapq.heappush(h, (x + nums[i + 1], i + 1))
                 heapq.heappush(h, (x - nums[i] + nums[i + 1], i + 1))
         return s - x
+
+
+class SolutionB(Solution):
+
+    @override
+    def k_sum(self, nums: list[int], k: int) -> int:
+        n = len(nums)
+
+        def check(x: int) -> bool:
+            cnt = 1
+
+            def dfs(i: int, s: int):
+                nonlocal cnt
+                if i == n or s + nums[i] > x or cnt == k:
+                    return
+                cnt += 1
+                dfs(i + 1, s)
+                dfs(i + 1, s + nums[i])
+
+            dfs(0, 0)
+            return cnt == k
+
+        s = 0
+        for i, x in enumerate(nums):
+            if x >= 0:
+                s += x
+            else:
+                nums[i] = -x
+        nums.sort()
+        return s - bisect_left(range(sum(nums)), True, key=check)
