ad4a71ac48292d41507d66323c31273407f446ad
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-07-01 01:15:20 +0000

    74. search a 2D matrix: binary search

--- src/leetcode/n74/solution.py
+++ src/leetcode/n74/solution.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from typing import override
 
 
 class Solution(ABC):
@@ -6,3 +7,20 @@ class Solution(ABC):
     @abstractmethod
     def search_matrix(self, matrix: list[list[int]], target: int) -> bool:
         pass
+
+
+class SolutionA(Solution):
+
+    @override
+    def search_matrix(self, matrix: list[list[int]], target: int) -> bool:
+        for row in matrix:
+            l, r = -1, len(row)
+            while l + 1 < r:
+                i = (l + r) // 2
+                if row[i] == target:
+                    return True
+                if row[i] < target:
+                    l = i
+                else:
+                    r = i
+        return False
--- test/leetcode/n74/test_solution.py
+++ test/leetcode/n74/test_solution.py
@@ -1,4 +1,4 @@
-from src.leetcode.n74.solution import Solution
+from src.leetcode.n74.solution import Solution, SolutionA
 from unittest import TestCase
 
 
@@ -13,3 +13,9 @@ class SolutionTest(TestCase):
 
     def test_2(self):
         self.assertFalse(self.solution.search_matrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 13))
+
+
+class SolutionATest(SolutionTest):
+
+    def setUp(self):
+        self.solution = SolutionA()
