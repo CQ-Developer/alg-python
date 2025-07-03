9679f19fc8a766f8813ff85f5acf0400073661c2
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-07-03 09:25:35 +0000

    154. find minimum in rotated sorted array II

---
+++ test/leetcode/n154/test_solution.py
@@ -0,0 +1,18 @@
+from src.leetcode.n154.solution import Solution
+from unittest import TestCase, SkipTest
+
+
+class TestSolution(TestCase):
+    solution: Solution
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        if cls is TestSolution:
+            raise SkipTest('skip')
+        return super().setUpClass()
+
+    def test_1(self):
+        self.assertEqual(1, self.solution.find_min([1, 3, 5]))
+
+    def test_2(self):
+        self.assertEqual(0, self.solution.find_min([2, 2, 2, 0, 1]))
