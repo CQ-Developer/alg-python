d9b7bed208412fe427a2f4afd8f45bfb10ba9bf4
Jack <8551681083@163.com>
2025-07-12 07:30:03 +0000

    222. count complete tree nodes

---
+++ test/leetcode/n222/test_solution.py
@@ -0,0 +1,23 @@
+from src.leetcode.n222.solution import TreeNode, Solution
+from unittest import TestCase, SkipTest
+
+
+class TestSolution(TestCase):
+    solution: Solution
+
+    @classmethod
+    def setUpClass(cls):
+        if cls is TestSolution:
+            raise SkipTest('skip')
+        super().setUpClass()
+
+    def test_1(self):
+        root = TreeNode(1, TreeNode(2, TreeNode(4), TreeNode(5)), TreeNode(3, TreeNode(6)))
+        self.assertEqual(6, self.solution.count_nodes(root))
+
+    def test_2(self):
+        self.assertEqual(0, self.solution.count_nodes(None))
+
+    def test_3(self):
+        root = TreeNode(1)
+        self.assertEqual(1, self.solution.count_nodes(root))
