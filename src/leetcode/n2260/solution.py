2d6b4075ac8bd4d949c69007ff6cd061c349e8a7
chen qiang <chen_qiang@hoperun.com>
2025-08-06 14:31:39 +0800

    2260. Minimum Consecutive Cards to Pick Up hash table

--- src/leetcode/n2260/solution.py
+++ src/leetcode/n2260/solution.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+from math import inf, isinf
+from typing import override
 
 
 class Solution(ABC):
@@ -6,3 +8,16 @@ class Solution(ABC):
     @abstractmethod
     def minimum_card_pickup(self, cards: list[int]) -> int:
         pass
+
+
+class SolutionA(Solution):
+
+    @override
+    def minimum_card_pickup(self, cards: list[int]) -> int:
+        latest = dict()
+        res = inf
+        for i, x in enumerate(cards):
+            if x in latest:
+                res = min(res, i - latest[x] + 1)
+            latest[x] = i
+        return -1 if isinf(res) else res
--- test/leetcode/n2260/test_solution.py
+++ test/leetcode/n2260/test_solution.py
@@ -1,6 +1,6 @@
 from unittest import TestCase, SkipTest
 
-from src.leetcode.n2260.solution import Solution
+from src.leetcode.n2260.solution import Solution, SolutionA
 
 
 class TestSolution(TestCase):
@@ -10,10 +10,15 @@ class TestSolution(TestCase):
     def setUpClass(cls):
         if cls is TestSolution:
             raise SkipTest('skip')
-        print('!!!!!!!!!!!!!!!!!!!!!')
 
     def test_1(self):
         self.assertEqual(4, self.solution.minimum_card_pickup([3, 4, 2, 3, 4, 7]))
 
     def test_2(self):
         self.assertEqual(-1, self.solution.minimum_card_pickup([1, 0, 5, 3]))
+
+
+class TestSolutionA(TestSolution):
+
+    def setUp(self):
+        self.solution = SolutionA()
