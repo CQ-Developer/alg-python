9c7683631546181f9dad6ca245dcf0fd106b6c39
Jack <8551681083@163.com>
2025-08-06 15:14:32 +0000

    2815. Max Pair Sum in an Array

---
+++ test/leetcode/n2815/test_solution.py
@@ -0,0 +1,20 @@
+from src.leetcode.n2815.solution import Solution
+from unittest import TestCase, SkipTest
+
+
+class TestSolution(TestCase):
+    solution: Solution
+
+    @classmethod
+    def setUpClass(cls):
+        if cls is TestSolution:
+            raise SkipTest('skip')
+
+    def test_1(self):
+        self.assertEqual(-1, self.solution.max_sum([112, 131, 411]))
+
+    def test_2(self):
+        self.assertEqual(5902, self.solution.max_sum([2536, 1613, 3366, 162]))
+
+    def test_3(self):
+        self.assertEqual(88, self.solution.max_sum([51, 71, 17, 24, 42]))
