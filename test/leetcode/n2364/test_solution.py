8e8248af58b43576cded0d181ed4d1fbd77e2391
Jack <8551681083@163.com>
2025-08-09 08:59:57 +0000

    2364. Count Number of Bad Pairs

---
+++ test/leetcode/n2364/test_solution.py
@@ -0,0 +1,17 @@
+from src.leetcode.n2364.solution import Solution
+from unittest import TestCase, SkipTest
+
+
+class TestSolution(TestCase):
+    solution: Solution
+
+    @classmethod
+    def setUpTest(cls):
+        if cls is TestSolution:
+            raise SkipTest('skip')
+
+    def test1(self):
+        self.assertEqual(5, self.solution.count_bad_pairs([4, 1, 3, 3]))
+
+    def test2(self):
+        self.assertEqual(0, self.solution.count_bad_pairs([1, 2, 3, 4, 5]))
