eeea161249c05bf00bb13051ef97d6cb54a9307c
Jack <18551681083@163.com>
2025-08-14 05:22:35 +0000

    2555. Maximize Win From Two Segments

--- src/leetcode/n2555/solution.py
+++ src/leetcode/n2555/solution.py
@@ -24,3 +24,20 @@ class SolutionA(Solution):
             res = max(res, mx[l] + r - l + 1)
             mx[r + 1] = max(mx[r], r - l + 1)
         return res
+
+
+class SolutionB(Solution):
+
+    def maximize_win(self, prize_positions: list[int], k: int) -> int:
+        n = len(prize_positions)
+        if k * 2 + 1 >= prize_positions[-1] - prize_positions[0]:
+            return n
+        res = mx = l = r = 0
+        for i, p in enumerate(prize_positions):
+            while r < n and prize_positions[r] - p <= k:
+                r += 1
+            res = max(res, mx + r - i)
+            while p - prize_positions[l] > k:
+                l += 1
+            mx = max(mx, i - l + 1)
+        return res
