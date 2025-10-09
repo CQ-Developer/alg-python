8beac7ca109ff07f48cafe0cae9e0de1e4dcba2c
Jack <18551681083@163.com>
2025-10-09 05:47:28 +0000

    3113. Find the Number of Subarrays Where Boundary Elements Are Maximum 单调栈

--- src/leetcode/n3113/solution.py
+++ src/leetcode/n3113/solution.py
@@ -1,7 +1,27 @@
 from abc import ABC, abstractmethod
+from typing import override
+from math import inf
+
 
 class Solution(ABC):
 
     @abstractmethod
-    def numberOfSubarrays(self, nums:list[int])-> int:
+    def numberOfSubarrays(self, nums: list[int]) -> int:
         pass
+
+
+class SolutionA(Solution):
+
+    @override
+    def numberOfSubarrays(self, nums: list[int]) -> int:
+        n = len(nums)
+        stk = [[inf, 0]]
+        for num in nums:
+            while num > stk[-1][0]:
+                stk.pop()
+            if num == stk[-1][0]:
+                n += stk[-1][1]
+                stk[-1][1] += 1
+            else:
+                stk.append([num, 1])
+        return n
--- test/leetcode/n3113/test_solution.py
+++ test/leetcode/n3113/test_solution.py
@@ -1,5 +1,6 @@
-from src.leetcode.n3113.solution import Solution
+from src.leetcode.n3113.solution import Solution, SolutionA
 from unittest import TestCase, SkipTest
+
 
 class TestSolution(TestCase):
     solution: Solution
@@ -7,13 +8,19 @@ class TestSolution(TestCase):
     @classmethod
     def setUpClass(cls):
         if cls is TestSolution:
-            raise SkipTest('skip')
-    
+            raise SkipTest('abstract')
+
     def test_1(self):
-        self.assertEqual(6, self.solution.numberOfSubarrays([1,4,3,3,2]))
-    
+        self.assertEqual(6, self.solution.numberOfSubarrays([1, 4, 3, 3, 2]))
+
     def test_2(self):
-        self.assertEqual(6, self.solution.numberOfSubarrays([3,3,3]))
+        self.assertEqual(6, self.solution.numberOfSubarrays([3, 3, 3]))
 
     def test_3(self):
         self.assertEqual(1, self.solution.numberOfSubarrays([1]))
+
+
+class TestSolutionA(TestSolution):
+
+    def setUp(self):
+        self.solution = SolutionA()
