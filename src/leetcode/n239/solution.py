f70cc8a8025c67ab14a86e9d453ad3f023d29e49
Jack <18551681083@163.com>
2025-10-22 09:54:00 +0000

    239. Sliding Window Maximum

--- src/leetcode/n239/solution.py
+++ src/leetcode/n239/solution.py
@@ -14,14 +14,13 @@ class SolutionA(Solution):
 
     @override
     def max_sliding_window(self, nums: list[int], k: int) -> list[int]:
-        dq: deque[int] = deque()
-        ans: list[int] = []
+        dq, ans = deque(), []
         for i, x in enumerate(nums):
-            if dq and dq[0] < i - k + 1:
-                dq.popleft()
             while dq and x > nums[dq[-1]]:
                 dq.pop()
             dq.append(i)
+            if dq[0] < i - k + 1:
+                dq.popleft()
             if i >= k - 1:
                 ans.append(nums[dq[0]])
         return ans
