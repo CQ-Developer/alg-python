e0ab9c924c392b3fa93ebd0774484c00dab99f02
Jack <18551681083@163.com>
2025-11-17 05:38:43 +0000

    2104. sum of subarray ranges

--- src/leetcode/n2104/solution.py
+++ src/leetcode/n2104/solution.py
@@ -38,10 +38,10 @@ class SolutionB(Solution):
             left, right = [0] * n, [n] * n
             stk = [-1]
             for i, x in enumerate(nums):
-                while len(stk) > 1 and nums[stk[-1]] >= x:
+                while len(stk) > 1 and nums[stk[-1]] <= x:
                     right[stk.pop()] = i
                 left[i] = stk[-1]
                 stk.append(i)
-            return sum((l - i) * (r - i) * x for i, (x, l, r) in enumerate(zip(nums, left, right)))
+            return sum((i - l) * (r - i) * x for i, (x, l, r) in enumerate(zip(nums, left, right)))
 
         return resolve(nums) + resolve([-x for x in nums])
