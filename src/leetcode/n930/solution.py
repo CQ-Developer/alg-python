069ed3ddcac769aa2eb934b541ede7e07b858af9
Jack <18551681083@163.com>
2026-06-22 09:43:24 +0800

    930. binary subarrays with sum

--- src/leetcode/n930/solution.py
+++ src/leetcode/n930/solution.py
@@ -5,6 +5,9 @@ from collections import defaultdict
 
 
 class Solution(ABC):
+    """
+    和相同的二元子数据
+    """
 
     @abstractmethod
     def num_subarrays_with_sum(self, nums: list[int], goal: int) -> int:
@@ -12,11 +15,29 @@ class Solution(ABC):
 
 
 class SolutionA(Solution):
+    """
+    前缀和
+    """
 
     @override
     def num_subarrays_with_sum(self, nums: list[int], goal: int) -> int:
-        ans = pre = 0
         cnt = defaultdict(int)
+        ans = 0
+        for x in accumulate(nums, initial=0):
+            ans += cnt[x - goal]
+            cnt[x] += 1
+        return ans
+
+
+class SolutionB(Solution):
+    """
+    前缀和: 一次遍历
+    """
+
+    @override
+    def num_subarrays_with_sum(self, nums: list[int], goal: int) -> int:
+        cnt = defaultdict(int)
+        ans = pre = 0
         for x in nums:
             cnt[pre] += 1
             pre += x
@@ -24,19 +45,21 @@ class SolutionA(Solution):
         return ans
 
 
-class SolutionB(Solution):
+class SolutionC(Solution):
+    """
+    滑动窗口
+    """
 
     @override
     def num_subarrays_with_sum(self, nums: list[int], goal: int) -> int:
-        cnt = 0
-        l1 = l2 = s1 = s2 = 0
+        cnt = l1 = l2 = s1 = s2 = 0
         for r, x in enumerate(nums):
             # s2 <= goal
             s2 += x
             while l2 <= r and s2 > goal:
                 s2 -= nums[l2]
                 l2 += 1
-            # s1  < goal
+            # s1 < goal
             s1 += x
             while l1 <= r and s1 >= goal:
                 s1 -= nums[l1]
