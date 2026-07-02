2cd75fe1a1c87515f10689df65a17c62786afd81
Jack <18551681083@163.com>
2026-07-02 14:54:58 +0800

    17.05 find longest subarray LCCI

--- src/leetcode/interview/n17/n05/solution.py
+++ src/leetcode/interview/n17/n05/solution.py
@@ -1,27 +1,35 @@
-from abc import ABC, abstractmethod
-from typing import override
+import abc
+import typing
 
 
-class Solution(ABC):
+class Solution(abc.ABC):
+    """
+    找到最长的子数组，且包含的字母和数字的个数相同。
+    返回左端点下标值最小的子数组。
+    """
 
-    @abstractmethod
+    @abc.abstractmethod
     def find_longest_subarray(self, array: list[str]) -> list[str]:
         pass
 
 
 class SolutionA(Solution):
+    """
+    前缀和
+    """
 
-    @override
+    @typing.override
     def find_longest_subarray(self, array: list[str]) -> list[str]:
-        p = n = pre = 0
-        cnt = {0: -1}
+        p = n = 0
+        pre = 0
+        table = {0: -1}
         for i, x in enumerate(array):
             pre += 1 if x.isdigit() else -1
-            if pre in cnt:
-                j = cnt[pre]
+            if pre in table:
+                j = table[pre]
                 m = i - j
                 if m > n:
                     p, n = j, m
             else:
-                cnt[pre] = i
+                table[pre] = i
         return array[p + 1 : p + 1 + n]
