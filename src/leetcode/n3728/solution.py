863ac0c8bcb70ae6dc599db8a6bcaec0911ef7c0
Jack <18551681083@163.com>
2026-07-24 08:57:56 +0800

    3728. stable subarrays with equal boundary and interior sum
    3728. stable subarrays with equal boundary and interior sum
    
    updatge doc

--- src/leetcode/n3728/solution.py
+++ src/leetcode/n3728/solution.py
@@ -5,6 +5,9 @@ from itertools import pairwise
 
 
 class Solution(ABC):
+    """
+    统计符合 a[l] = a[r] = sum( a[l + 1] ... a[r - 1]) 的数量
+    """
 
     @abstractmethod
     def count_stable_subarrays(self, capacity: list[int]) -> int:
@@ -12,6 +15,21 @@ class Solution(ABC):
 
 
 class SolutionA(Solution):
+    """
+    推导
+    - r - l + 1 >= 3
+    - cap[r] = cap[l]
+    - pre[r] = cap[l] + pre[l + 1]
+
+    结论
+    - r - l > 1
+    - (cap[r], pre[r]) = (cap[l], cap[l] + pre[l + 1])
+
+    补充
+    通过先计算 r，再更新 r - 1,
+    对于下一轮 r + 1 来说, (r + 1)-(r - 1) = 2 > 1
+    天然满足了长度要求
+    """
 
     @override
     def count_stable_subarrays(self, capacity: list[int]) -> int:
@@ -20,6 +38,6 @@ class SolutionA(Solution):
         cnt = defaultdict(int)
         for p, x in pairwise(capacity):
             ans += cnt[(x, pre)]
-            cnt[(p, pre + p)] += 1
+            cnt[(p, p + pre)] += 1
             pre += x
         return ans
