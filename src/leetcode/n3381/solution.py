39c0ef2571f0b309f4354876cb820088578d791c
Jack <18551681083@163.com>
2026-07-24 15:50:57 +0800

    3381. maximum subarray sum with length divisible by k

--- src/leetcode/n3381/solution.py
+++ src/leetcode/n3381/solution.py
@@ -1,25 +1,26 @@
 from abc import ABC, abstractmethod
-from typing import override
 from math import inf
+from typing import override
 
 
 class Solution(ABC):
-
     @abstractmethod
     def max_subarray_sum(self, nums: list[int], k: int) -> int:
         pass
 
 
 class SolutionA(Solution):
+    """
+    """
 
     @override
     def max_subarray_sum(self, nums: list[int], k: int) -> int:
-        min_pre = [inf] * (k + 1)
-        pre = min_pre[0] = 0
+        min_pre = [inf] * k
+        pre = min_pre[-1] = 0
         ans = -inf
-        for i, x in enumerate(nums, start=1):
+        for j, x in enumerate(nums):
             pre += x
-            j = i % k
-            ans = max(ans, pre - min_pre[j])
-            min_pre[j] = min(pre, min_pre[j])
+            i = j % k
+            ans = max(ans, pre - min_pre[i])
+            min_pre[i] = min(min_pre[i], pre)
         return ans
