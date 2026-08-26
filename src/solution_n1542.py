5856b615f97aff37ab52dbe73dae18299f69ab03
Jack <18551681083@163.com>
2026-08-26 10:52:50 +0800

    1542. Find Longest Awesome Substring

---
+++ src/solution_n1542.py
@@ -0,0 +1,35 @@
+import abc
+import typing
+
+
+class Solution(abc.ABC):
+    @abc.abstractmethod
+    def longest_awesome(self, s: str) -> int:
+        pass
+
+
+class SolutionA(Solution):
+    """
+    回文串长度为偶数
+        pos[j] ^ pos[i] == 0
+    回文串长度为奇数
+        ( pos[j] ^ pos[i] ).bit_count() == 1
+    """
+
+    @typing.override
+    def longest_awesome(self, s: str) -> int:
+        pos = {0: -1}
+        st = ans = 0
+        for i, x in enumerate(map(int, s)):
+            st ^= 1 << x
+            # 奇数
+            for j in range(10):
+                _st = st ^ (1 << j)
+                if _st in pos:
+                    ans = max(ans, i - pos[_st])
+            # 偶数
+            if st in pos:
+                ans = max(ans, i - pos[st])
+            else:
+                pos[st] = i
+        return ans
---
+++ test/test_n1542.py
@@ -0,0 +1,24 @@
+import pytest
+
+from src.solution_n1542 import Solution, SolutionA
+
+
+@pytest.fixture(scope="module", params=[SolutionA])
+def solution(request) -> Solution:
+    return request.param()
+
+
+def test_1(solution: Solution):
+    assert solution.longest_awesome("3242415") == 5
+
+
+def test_2(solution: Solution):
+    assert solution.longest_awesome("12345678") == 1
+
+
+def test_3(solution: Solution):
+    assert solution.longest_awesome("213123") == 6
+
+
+def test_4(solution: Solution):
+    assert solution.longest_awesome("00") == 2
